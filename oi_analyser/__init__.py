295ec1e284563d4a420aaeb428e5595e5e13c1c8
Keji Li <kli@acadian-asset.com>
2024-10-28 14:18:44 -0400

    fixed to use pyproject

--- oi_analyser/__init__.py
+++ oi_analyser/__init__.py
@@ -1,3 +1,7 @@
+"""A package to analyze the optical imaging results acquired by
+[CortexView2](https://github.com/Palpatineli/CortexView2)
+"""
+__version__ = "0.1.6"
 from .imaging import (convert, calculate, circular_transform, circular_free, detrend,
                       get_colormap, online_result, colorize)
 
